4d9008993bd22f5305f3ace5eaaab29d0cfb91bd
danpollak <dandan888@gmail.com>
2018-06-28 11:55:38 +0300

    added comments, threw away unused code

--- components/spawn.js
+++ components/spawn.js
@@ -1,45 +1,42 @@
-if (typeof AFRAME === 'undefined') {
-  throw new Error(
-    'Component attempted to register before AFRAME was available.'
-  )
-}
-
-const setComponentProperty = AFRAME.utils.entity.setComponentProperty
-
 AFRAME.registerComponent('spawn', {
-  // Allows for multiple instances of animations
-  dependencies: ['tag'],
-
   schema: {},
 
   init: function () {
+    // binding all external methods to this
     this.bindMethods()
-    this.editor = this.el.sceneEl.getAttribute('editor') === 'true'
-    this.setListener = false
+    // this will hold the x,y positions of the tapping
     this.tapData = null
 
-    // make the object invisible on start
-    this.el.setAttribute('visible', false);
+    // adding a listener to the window itself to get the tapping data
     window.addEventListener('touchstart', this.onTouchStart)
+
+    // adding a listener to the scene to handle the frame updates of the AR scene
     this.el.sceneEl.addEventListener('updateFrame', this.handleSpawn)
 
   },
 
   handleSpawn: function (data) {
+    // reference to this
     const self = this
+    // reference to the callback data
     const frame = data.detail
+    // checking if a tap detected
     if (this.tapData !== null) {
-    const x = this.tapData[0];
-    const y = this.tapData[1];
-    this.tapData = null;
-    data.detail.findAnchor(x, y).then(function (anchorOffset) {
-        if (anchorOffset) {
-            // will set the position and orientation based on the anchorOffset attached to the entity
-            const anchor = self.el
-            anchor.setAttribute('xranchor', {})
-            anchor.components.xranchor.anchorOffset = anchorOffset
-            anchor.object3D.visible = true
-            }
+      // moving the tapping data to constants, then clearing the data to not repeat the same one
+      const x = this.tapData[0];
+      const y = this.tapData[1];
+      this.tapData = null;
+
+      // calling to the WebXR API method findAnchor, that looks for anchor from a x,y position on screen in scene
+      frame.findAnchor(x, y).then(function (anchorData) {
+        // if an anchor was found and created, attach it to the entity
+        if (anchorData) {
+            const entity = self.el
+            // add the "xranchor" component, which allows to attach an anchor by id to an entity
+            entity.setAttribute('xranchor', {})
+            // add the current anchor to the component
+            entity.components.xranchor.anchorOffset = anchorData
+          }
     })
     .catch(function (err) {
         console.error('Error in hit test', err)
